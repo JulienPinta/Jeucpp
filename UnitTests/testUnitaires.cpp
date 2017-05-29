0fda0df2f8fca9882747b093e5449d8f90ccff99
Faedryth <val_rockonly@hotmail.fr>
2017-05-29 19:27:15 +0200

    le reste

---
+++ UnitTests/testUnitaires.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "../Bibliotheque/Noisette.h"
+
+TEST(Noisette, getPosxWorks) {
+	noisette nois1(100,0);
+	EXPECT_EQ(100, nois1.getPosx());
+}
+
+TEST(Noisette, getPosyWorks) {
+	noisette nois1(0,50);
+	EXPECT_EQ(50, nois1.getPosy());
+}
